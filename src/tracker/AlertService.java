75ea3e63f1e38954f0bfad0c6a4353ed4e46f3a7
yogitachouha <yogita0210chouhan@gmail.com>
2025-10-21 22:58:06 +0530

    improved alert message formatting for better readability

--- src/tracker/AlertService.java
+++ src/tracker/AlertService.java
@@ -4,5 +4,6 @@ public class AlertService implements StockObserver{
 	@Override
 	 public void onLowStock(Product product) {
 	        System.out.println("⚠️ Restock Alert: Low stock for " + product.getName());
+	        System.out.println("--------------------------------------------------");
 	    }
 }
